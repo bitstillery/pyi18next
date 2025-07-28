694864b03ed5ab40964a38c27e1b6c677addbf22
J. van Veen <jvanveen@protonmail.com>
2025-07-28 11:49:11 +0200

    chore: cleanup build

--- bitstillery_pyi18next/__init__.py
+++ bitstillery_pyi18next/__init__.py
@@ -3,12 +3,8 @@ bitstillery_pyi18next - A Python implementation of i18next with pluralisation fi
 
 This is a fork of pyi18next that fixes pluralisation count issues.
 """
-
-__version__ = "0.0.3"
 __author__ = "Gongziting Tech Ltd., Bitstillery"
 
 from .i18next import I18next
-from .translate import translate
-from .functions import t
 
-__all__ = ["I18next", "translate", "t"]
+__all__ = ["I18next"]
